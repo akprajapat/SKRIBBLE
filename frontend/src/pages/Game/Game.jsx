9687b18af3f26d7e947dce19f8dbefb2f50728ca
ak <arpitp1712@gmail.com>
2025-09-02 19:26:21 +0530

    Game will be played in here, player can draw on its turn and guess and chat otherwise

---
+++ frontend/src/pages/Game/Game.jsx
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import { useSocket } from "../../context/SocketContext";
+import { useGame } from "../../context/GameContext";
+
+import Topbar from "../../components/Topbar/Topbar";
+import Canvas from "../../features/Canvas/Canvas";
+import Chat from "../../components/Chat/Chat";
+import PlayerList from "../../components/Playerlist/Playerlist";
+import WordChoice from "../../features/WordChoice/WordChoice";
+import Scoreboard from "../../features/Scoreboard/Scoreboard";
+
+import "./Game.css";
+
+export default function Game() {
+  const socket = useSocket();
+  const {
+    getRoomId,
+    getPlayers,
+    getWordChoices,
+    getScores,
+    getDrawerId,
+    getPlayerId,
+    getRound,
+    getTotalRounds,
+    getCurrentWord,
+    getTimer
+  } = useGame();
+
+  const roomId = getRoomId();
+  const playerId = getPlayerId();
+  const isDrawer = getDrawerId() === playerId;
+
+  useEffect(() => {
+    console.log("Socket connected:", socket);
+    socket.on("connect", () => {
+      console.log("Socket connected:", socket.id);
+    });
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
+
+  }, [socket]);
+
+  return (
+    <div className="Game">
+      <div className="Topbar-container">
+        <Topbar
+          round={getRound()}
+          totalRounds={getTotalRounds()}
+          word={getCurrentWord()}
+          timeLeft={getTimer()}
+        />
+      </div>
+      <div className="Canvas-container">
+        {/* {getWordChoices()?.length > 0 ? (
+          <WordChoice choices={getWordChoices()} />
+        ) : (
+        )} */}
+          <Canvas roomId={roomId} isDrawer={isDrawer} />
+      </div>
+      <div className="player-chat-container">
+        <PlayerList players={getPlayers()} />
+        <Chat />
+      </div>
+    </div> 
+  );
+}
