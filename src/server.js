406fce192ca21f81749cb266086835859badbe82
ak <arpitp1712@gmail.com>
2025-09-02 17:14:22 +0530

    create server using app and binding socket

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import http from 'http';
+import app from './app.js';
+import attachSocket from './socket.js';
+
+const server = http.createServer(app);
+const io = attachSocket(server);
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`✅ Server listening on http://localhost:${PORT}`);
+});
