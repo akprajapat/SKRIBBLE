fa5ab19cf0abafc095a480e3382104cf2c7da7a3
ak <arpitp1712@gmail.com>
2025-09-20 18:10:58 +0530

    update socket to use server url shared in env

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost";
-const PORT = import.meta.env.VITE_SOCKET_PORT || "4000";
+const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:4000";
 
-const socket = io(`${URL}:${PORT}`);
+console.log("Connecting to Socket.IO server at:", URL);
+
+const socket = io(URL);
 
 export default socket;
